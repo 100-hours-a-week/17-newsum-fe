a0dd64f94f092fa4fe99e62c930a7f22e8c60713
gongsua <91416981+gongsua@users.noreply.github.com>
2025-05-06 17:37:19 +0900

    replace hardcoded urls with env values in DefaultAxios and TokenAxios

--- src/api/DefaultAxios.jsx
+++ src/api/DefaultAxios.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// Token 담지 않고 요청하는 경우
 const DefaultAxios = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
@@ -8,24 +7,14 @@ const DefaultAxios = axios.create({
   },
 });
 
-// 요청 인터셉터
 DefaultAxios.interceptors.request.use(
-  (config) => {
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (config) => config,
+  (error) => Promise.reject(error)
 );
 
-// 응답 인터셉터
 DefaultAxios.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (response) => response,
+  (error) => Promise.reject(error)
 );
 
-export default DefaultAxios; 
+export default DefaultAxios;
