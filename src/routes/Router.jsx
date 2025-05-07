1efe260309a51b7a8bc9d153120d4407b50214b1
gongsua <91416981+gongsua@users.noreply.github.com>
2025-05-07 09:36:55 +0900

    router수정 및 환경변수 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,6 +5,8 @@ import MainLayout from '../components/Layout/MainLayout';
 import HomePage from '../pages/HomePage';
 import ArticlePage from '../pages/ArticlePage';
 import LoginPage from '../pages/LoginPage';
+import CommentPage from '../pages/CommentPage';
+import CategoryPage from '../pages/CategoryPage';
 import MyProfilePage from '../pages/MyProfilePage';
 import GoogleRedirectHandler from '../pages/GoogleRedirectHandler';
 import KakaoRedirectHandler from '../pages/KakaoRedirectHandler';
@@ -17,14 +19,22 @@ function Router() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/oauth2/callback/google" element={<GoogleRedirectHandler />} />
       <Route path="/oauth2/callback/kakao" element={<KakaoRedirectHandler />} />
+      <Route path='/comment/:articleId' element={<CommentPage />} />
 
       {/* ⭐️ 기존 헤더/바텀네비 없는 단독 페이지로 분리 ⭐️ */}
       <Route path="/users/profile" element={<MyProfilePage />} />
 
-      {/* MainLayout 아래에 나머지 페이지들 */}
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="article/:articleId" element={<ArticlePage />} />
+   
+      {/* MainLayout: 헤더/푸터 있는 공통 레이아웃 */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/category/:category_id" element={<CategoryPage />} />
+      </Route>
+
+
+      <Route element={<NoHeaderLayout />}>
+        <Route path='/article/:articleID' element={<ArticlePage />}/>
+      
       </Route>
     </Routes>
   );
