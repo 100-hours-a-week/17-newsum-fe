4d49ceb9641ae82e0b1309c2ecdde6f4503989f0
gongsua <91416981+gongsua@users.noreply.github.com>
2025-06-17 13:31:43 +0900

    feat : 네비바 북마크 불들어오도록 변경

--- src/components/dropdown/CategoryDropdown.jsx
+++ src/components/dropdown/CategoryDropdown.jsx
@@ -34,7 +34,7 @@ const MenuItem = styled(Button)`
   width: 100%;
   padding: 16px;
   text-align: center;
-  color: ${props => props.theme.palette.text.primary};
+  color: black;
   font-weight: 500;
   border-bottom: 1px solid rgba(0, 0, 0, 0.08);
   background-color: ${props => props.selected ? 'rgba(0, 0, 0, 0.04)' : 'transparent'};
@@ -132,6 +132,7 @@ function CategoryDropdown({ selectedCategory, onCategoryChange }) {
                   sx={{
                     opacity: 1,
                     cursor: 'pointer',
+                    color: 'black',
                   }}
                 >
                   {cat}
